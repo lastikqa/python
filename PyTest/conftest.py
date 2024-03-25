ade8bf2c4412434232d44b5340c73ca28a43de65
Lastik1595 <139847710+Lastik1595@users.noreply.github.com>
2024-03-25 17:48:06 +0500

    updated pytests

--- PyTest/conftest.py
+++ PyTest/conftest.py
@@ -1,17 +1,36 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="ru",
+                     help="Choose language: '--language=en' or '--language=ru'")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")  # to disable "im a robot"
-    browser = webdriver.Chrome(options=options)
+def language(request) :
+    language = request.config.getoption("language")
+    return language
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name",)
+    language = request.config.getoption("language")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("prefs", {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=chrome_options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options = Options()
+        options.set_preference(f'intl.accept_languages',f'{language}')
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
--- PyTest/test_items.py
+++ PyTest/test_items.py
@@ -1,34 +1,24 @@
+
 import time
 
 import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function")
-def browser() :
-    print("\nstart browser for test..")
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")  # to disable "im a robot"
-    browser = webdriver.Chrome(options=options)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
-
-@pytest.mark.parametrize('language', ["ru", "en-gb","fr"])
-def test_localisation(browser, language) :
-    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+def test_localisation(browser,language) :
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
     browser.get(link)
     wait = WebDriverWait(browser, 30, poll_frequency=1)
     add_to_basket = wait.until(EC.element_to_be_clickable(browser.find_element(By.XPATH, "(//button)[3]"))).text
+    print(language)
+
     if language == "ru" :
         assert add_to_basket == "Добавить в корзину", f"Your localisation must be ru, now your is {language} "
     elif language == 'en-gb' :
         assert add_to_basket == "Add to basket", f"Your localisation must be en-gb, now your is {language} "
     else :
-         print(f"  {language} is not suppotred, u might have isues.")
+        print(f"  {language} is not suppotred, u might have isues.")
     time.sleep(5)
