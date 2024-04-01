743f22fae11c95acb6a08d786431fbd4402d89a8
Lastik1595 <139847710+Lastik1595@users.noreply.github.com>
2024-04-01 19:11:42 +0500

    modified tests

--- PyTest/pages/base_page.py
+++ PyTest/pages/base_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
@@ -6,6 +7,7 @@ from PyTest.pages.locators import BasePageLocators
 from PyTest.pages.test_data import Language
 from selenium.webdriver.support.select import Select
 from PyTest.pages.test_data import SearchItem
+import math
 
 
 class BasePage:
@@ -25,10 +27,10 @@ class BasePage:
         self.browser = browser
         self.url = url
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=2):
         self.browser = browser
         self.url = url
-        WebDriverWait(self.browser, timeout, poll_frequency=1)
+        self.browser.implicitly_wait(timeout)
 
     def is_element_present(self, how, what):
         try:
@@ -36,6 +38,21 @@ class BasePage:
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException :
+            print("No second alert presented")
+
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
