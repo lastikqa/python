b3d2d8e8baf421bf2308de233cf6ffda9e6653c3
lastikqa <139847710+lastikqa@users.noreply.github.com>
2024-06-05 15:51:24 +0500

    Update config.py

--- english_bot/config.py
+++ english_bot/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 datebase_name = "english_bot_database.db"
 load_dotenv()
+'''instead of "os.getenv("token")" u can use token="your_token" or u can add '.env' file with token="your_token" and put into the root od the project'''
 token=os.getenv("token")
