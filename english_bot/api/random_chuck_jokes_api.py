ef953e9f318c010cb41c39524d327200a45d2d33
lastikqa <139847710+lastikqa@users.noreply.github.com>
2024-06-15 18:22:56 +0500

    Delete english_bot directory

--- english_bot/api/random_chuck_jokes_api.py
+++
@@ -1,2 +0,0 @@
-class RandomChuckJokesApi:
-    chuck_url="https://geek-jokes.sameerkumar.website/api?format=json"
--- english_bot/filters/abnormal_verbs_filter.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import CallbackQuery
-
-
-def abnormal_verbs_filter(callback: CallbackQuery) -> bool:
-    """"""
-    abnormal_verbs_callbacks = "abnormal_verbs"
-    message = callback.data
-    print(message)
-    return message == abnormal_verbs_callbacks
