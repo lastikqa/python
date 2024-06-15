e605d770a9553c46e92fdd95a75b46578d6be4ad
lastikqa <139847710+lastikqa@users.noreply.github.com>
2024-06-15 18:22:37 +0500

    Delete PyTest directory

--- PyTest/pages/test_data.py
+++
@@ -1,7 +0,0 @@
-class BasePageTestData:
-    chosen_language = ("British English", "en-gb")
-    search_item = "coders at work"
-
-class loginPageTestData:
-    password="testuser25"
-    email="test-user@gmail.com"
